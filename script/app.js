fa03b53c5321b7416eda5fdf160ed924d857bef3
alessia vinnik <alessia.vinnik@student.howest.be>
2022-10-06 15:33:43 +0200

    passwoord input klaar

--- script/app.js
+++ script/app.js
@@ -1,9 +1,17 @@
+
+
 function handleFloatingLabel() {
    
 }
 
 function handlePasswordSwitcher() {
-    
+    const passWordOptions = ['password', 'text'];
+    const input = document.querySelector('.js-check')
+    const svg_check = document.querySelector('.js-check_svg')
+    svg_check.addEventListener('change', function(){
+        console.log("geklikt")
+        input.type = passWordOptions[+this.checked];
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
