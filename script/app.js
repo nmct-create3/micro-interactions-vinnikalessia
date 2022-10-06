36b271fa588eb562f50243c664b37ae932797220
alessia vinnik <alessia.vinnik@student.howest.be>
2022-10-06 16:49:26 +0200

    demo

--- script/app.js
+++ script/app.js
@@ -1,7 +1,22 @@
-
+let email = {}, password = {}, signInButton;
 
 function handleFloatingLabel() {
    
+}
+
+function enableListeners(){
+    addEventListener('blur', function(){
+        console.info("blur event")
+    })
+    addEventListener('click', function(){
+        
+    })
+}
+
+function getDOMElements(){
+    email.field = document.querySelector('.js-email-field')
+    email.input = document.querySelector('.js-email-input')
+    email.errmsg = document.querySelector('.js-email-errormsg')
 }
 
 function handlePasswordSwitcher() {
@@ -9,7 +24,6 @@ function handlePasswordSwitcher() {
     const input = document.querySelector('.js-check')
     const svg_check = document.querySelector('.js-check_svg')
     svg_check.addEventListener('change', function(){
-        console.log("geklikt")
         input.type = passWordOptions[+this.checked];
     })
 }
@@ -18,4 +32,42 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
     handleFloatingLabel();
     handlePasswordSwitcher();
+    getDOMElements();
 });
+
+
+
+
+
+
+
+
+
+/****** DEMO ******/
+/*
+const checkinput = function(event){
+    if(this.target.value.length < 2 || this.target.value.length < 4){
+            inputelement.removeeventlistener('input', checkinput)
+        }
+}
+
+const listentoinput = function(inputelement){
+    inputelement.addeventlistener('blur', function(){
+        if(this.target.value.length < 2 || this.target.value.length < 4){
+            
+        }
+        else{
+            console.log("error, pls fix")
+            inputelement.addeventlistener('input', checkinput)
+        }
+    }) 
+    document.addeveentlistener('DOMContentLoaded', function(){
+        const input = document.queryselector('input')
+        listentoinput(input)
+    })
+}
+
+
+
+
+*/
