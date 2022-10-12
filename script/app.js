faee495f4e418840d466b9502baacc0f54301a94
alessia vinnik <alessia.vinnik@student.howest.be>
2022-10-12 14:19:37 +0200

    hartje animation bezig

--- script/app.js
+++ script/app.js
@@ -1,4 +1,5 @@
 let email = {}, password = {}, signInButton;
+let htmlInputMail;
 
 function handleFloatingLabel() {
    
@@ -12,11 +13,36 @@ function enableListeners(){
         
     })
 }
+/**********************************************************/
+const checkinput = function(event){
+    if(this.target.value.length < 2 || this.target.value.length < 4){
+            inputelement.removeeventlistener('input', checkinput)
+        }
+}
 
 function getDOMElements(){
-    email.field = document.querySelector('.js-email-field')
-    email.input = document.querySelector('.js-email-input')
-    email.errmsg = document.querySelector('.js-email-errormsg')
+    // htmlInputMail.innerHTML = `niets ingevuld`;
+    let htmlString = "";
+    if(inputMail.value == 0){
+        htmlString = "niets ingevuld";
+        htmlInputMail.innerHTML = "Email is not valid";
+        htmlstring += `<p class="c-form-field has-error">
+        <label class="c-label" for="username">
+            Email
+            <span class="js-email-errormsg"></span>
+        </label>
+        <input class="c-input" type="email" name="username"
+               id="username"
+               required>
+    </p>`
+    }
+    else{
+        htmlInputMail.innerHTML = "Email is valid";
+    }
+    htmlInputMail.innerHTML = htmlString;
+    // email.field = document.querySelector('.js-email-field')
+    // email.input = document.querySelector('.js-email-input')
+    // email.errmsg = document.querySelector('.js-email-errormsg')
 }
 
 function handlePasswordSwitcher() {
@@ -29,9 +55,11 @@ function handlePasswordSwitcher() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    htmlInputMail = document.querySelector('.js-email-input');
+
     console.log('Script loaded!');
     handleFloatingLabel();
-    handlePasswordSwitcher();
+    handlePasswordSwitcher(); /*het oogje svg*/
     getDOMElements();
 });
 
